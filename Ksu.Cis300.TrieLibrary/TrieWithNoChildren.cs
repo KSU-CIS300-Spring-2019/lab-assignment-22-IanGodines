6412bd735cd89aeff2107b1cb2d4e25f6bbf2e5d
godines <godines@ksu.edu>
2019-03-25 15:19:33 -0500

    nah

---
+++ Ksu.Cis300.TrieLibrary/TrieWithNoChildren.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ksu.Cis300.TrieLibrary
+{
+    public class TrieWithNoChildren : ITrie
+    {
+        private bool _emptyString = false;
+
+        public ITrie Add(string s)
+        {
+            if (s == "")
+            {
+                _emptyString = true;
+            }
+            else if (s[0] < 'a' || s[0] > 'z')
+            {
+                throw new ArgumentException();
+            }
+            else
+            {
+                int loc = s[0] - 'a';
+                if (_children[loc] == null)
+                {
+                    _children[loc] = new TrieWithOneChild();
+                }
+                _children[loc] = _children[loc].Add(s.Substring(1));
+            }
+            return this;
+        }
+    
+
+        public bool Contains(string s)
+        {   
+            if (s == "")
+            {
+                return _emptyString;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+    }
+}
---
+++ Ksu.Cis300.TrieLibrary/TrieWithOneChild.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ksu.Cis300.TrieLibrary
+{
+    public class TrieWithOneChild : ITrie
+    {
+        private bool _emptyString = false;
+
+        private ITrie[] _onlychild = new ITrie[26];
+
+        private char _label;
+
+        public TrieWithOneChild(string s, bool x)
+        {
+            if (s == "" || s[0] > 'z' || s[0] < 'a')
+            {
+                throw new Exception();
+            }
+        }
+ 
+        public ITrie Add(string s)
+        {
+            if (s == "")
+            {
+                _emptyString = true;
+            }
+            else if (s[0] == _label)
+            {
+                _onlychild[s[0] - 'a'].Add(s.Substring(1));
+                return this;
+            }
+            else
+            {
+                _onlychild[s[0] - 'a'] = new TrieWithManyChildren(s,_emptyString,_label,_children);
+            }
+        }
+
+        public bool Contains(string s)
+        {
+            if (s == "")
+            {
+                return _emptyString;
+            }
+            else
+            {
+                int loc = s[0] - 'a';
+                if (s[0] == _label)
+                {
+                    return _onlychild[loc].Contains(s.Substring(1)); ;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+
+    }
+}
